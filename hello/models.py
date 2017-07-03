19d0529b251472e6236eb4c7aec01b08f11f9764
Michael Wittig <mwittig@gmail.com>
2017-07-03 15:28:37 -0500

    added forecast

--- hello/models.py
+++ hello/models.py
@@ -20,9 +20,11 @@ class City(models.Model):
 	weather = models.CharField(max_length=100,default="Unknown")
 	temperature = models.IntegerField(default=290)
 	modified_date = models.DateTimeField(auto_now=True)
+	low_temperature_tomorrow = models.IntegerField(default=290) 
+	high_temperature_tomorrow = models.IntegerField(default=290) 
 	
 	def get_details(self):
-		if(timezone.now() - self.modified_date > datetime.timedelta(minutes=10) or True):
+		if(timezone.now() - self.modified_date > datetime.timedelta(minutes=10)):
 			try:
 				result = json.loads(self.pull_from_api())
 				if "weather" in result:
@@ -31,7 +33,15 @@ class City(models.Model):
 					self.name = result["name"]
 				if "main" in result:
 					self.temperature = round(float(result["main"]["temp"]))
-					self.save()
+					
+					
+				forecast = json.loads(self.get_forecast())
+				if "list" in forecast:
+					tomorrow = forecast["list"][9]
+					self.low_temperature_tomorrow = round(float(tomorrow["main"]["temp_min"]))
+					self.high_temperature_tomorrow = round(float(tomorrow["main"]["temp_max"]))
+				
+				self.save()
 			except:
 				logger = logging.getLogger(__name__)
 				logger.error("Failed to read from API")
@@ -39,9 +49,16 @@ class City(models.Model):
 	def pull_from_api(self):
 		r = requests.get("http://api.openweathermap.org/data/2.5/weather?id=" + str(self.api_id) + "&appid=cdfdac6c8b96346016dea9fa93d524b6")
 		return r.content
-		
+	
+	def get_forecast(self):
+		r = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=" + str(self.api_id) + "&appid=cdfdac6c8b96346016dea9fa93d524b6")
+		return r.content
+	
 	def temperature_in_f(self):
 		return self.k2f(self.temperature)
 
+	def tomorrow(self):
+		return "Tomorrow: " + str(self.k2f(self.low_temperature_tomorrow)) + "-" + str(self.k2f(self.high_temperature_tomorrow)) + " degrees F."
+		
 	def k2f(self, t):
 		return round((t*9/5.0)-459.67)
